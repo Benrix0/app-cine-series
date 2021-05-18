4ba3ff6b49eab9c142c61ce20cb8b245b74146cb
Benrix0 <benrix00@gmail.com>
2021-05-19 01:07:27 +0200

    Création du backend

--- back/app.js
+++ back/app.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 
 const Content = require('./models/content');
 
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://Benrix0:${process.env.PSWDMONGODB}@cluster0.kx2wg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+mongoose.connect(`mongodb+srv://Benrix0:${process.env.PSWDMONGODB}@cluster0.kx2wg.mongodb.net/cine-series?retryWrites=true&w=majority`,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -15,16 +15,25 @@ mongoose.connect(`mongodb+srv://Benrix0:${process.env.PSWDMONGODB}@cluster0.kx2w
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.post('/add-content', (req, res) => {
-    delete req.body._id;
 
+app.post('/add-content', (req, res) => {
     const content = new Content({
-        ...req.body
+        ...req.query
     });
 
     content.save()
         .then(() => res.status(201).json({ message: 'Content registered'}))
         .catch(error => res.status(400).json({ error }));
+})
+
+app.get('/get-content', (req, res) => {
+    Content.find()
+        .then(content => {
+            res.status(200).json(content)
+        })
+        .catch(error => {
+            res.status(400).json({ error })
+        })
 })
 
 module.exports = app;
--- back/models/content.js
+++ back/models/content.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const contentSchema = mongoose.Schema({
     title: {type: 'string', required: true},
     description: {type: 'string', required: true},
+    platforms: {type: 'array', required: true},
     images: {type: 'array', required: true}
 });
 
