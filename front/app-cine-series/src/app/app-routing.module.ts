6b1525004ff3daae2c6ade1dea90538f784d245a
Benrix0 <benrix00@gmail.com>
2021-05-18 20:47:59 +0200

    commit 1

--- front/app-cine-series/src/app/app-routing.module.ts
+++ front/app-cine-series/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddContentComponent } from './add-content/add-content.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { SeeContentItemPageComponent } from './see-content-item-page/see-content-item-page.component';
 import { SeeContentComponent } from './see-content/see-content.component';
 
 const routes: Routes = [
   {path: '', component: MainPageComponent},
   {path: 'see-content', component: SeeContentComponent},
+  {path: 'see-content/content/:id', component: SeeContentItemPageComponent},
   {path: 'add-content', component: AddContentComponent}
 ];
 
