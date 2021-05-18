1cccc86a56931ef5e3c01276ed7ce631a84740d5
Benrix0 <benrix00@gmail.com>
2021-05-18 21:21:23 +0200

    commit 1

--- back/app.js
+++ back/app.js
@@ -1,5 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const Content = require('./models/content');
+
+require('dotenv').config()
 
 const app = express();
+
+mongoose.connect(`mongodb+srv://Benrix0:${process.env.PSWDMONGODB}@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.post('/add-content', (req, res) => {
+    delete req.body._id;
+
+    const content = new Content({
+        ...req.body
+    });
+
+    content.save()
+        .then(() => res.status(201).json({ message: 'Content registered'}))
+        .catch(error => res.status(400).json({ error }));
+})
 
 module.exports = app;
